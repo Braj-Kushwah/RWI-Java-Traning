6ff917eb32bdb07f7371585d04956935153cdbb9
Brajesh Kushwah <brajesh30082002@gmail.com>
2024-04-27 13:35:52 +0530

    pattern added

--- Core Java/Pattern Printing/PlusPatternDemo4.java
+++ Core Java/Pattern Printing/PlusPatternDemo4.java
@@ -1,8 +1,8 @@
-public class PlusPatternDemo3 {
+public class PlusPatternDemo4 {
     public static void main(String[] args) {
-        for(int i =1;i<=7;i++){
+        for(int i = 1;i<=7;i++){
             for(int j=1;j<=7;j++){
-                if(i==4||j==4){
+                if(i==1 ||i==4 ||i==7 ||j==1 ||j==4 ||j==7){
                     System.out.print("+");
                 }else{
                     System.out.print(" ");
